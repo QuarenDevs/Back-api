74883ffc9ab9b9d4a4af9e76fc4907bb4d0b95cb
Carlos <np.carlos@gmail.com>
2020-07-19 15:49:29 -0500

    Inclusión de .env para recibir parámetros de configuración

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import 'dotenv/config'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const port:number = parseInt(process.env.PORT) || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  Logger.log(`Server is ready on http://localhost:${port}`)
 }
 bootstrap();
