edc83a6f67c924dd005cee2768d5f51c7ee449e9
Carlos <np.carlos@gmail.com>
2020-07-19 16:25:13 -0500

    Creación de API-docs con Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,25 @@ import 'dotenv/config'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const port:number = parseInt(process.env.PORT) || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const swaggerConfig = new DocumentBuilder()
+      .setTitle('App de emprendimiento API')
+      .setDescription('API de la app de emprendimiento organizada por módulos funcionales de las empresas')
+      .setVersion('1.0')
+      .build();
+  
+  const doc = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api-docs', app, doc);
+
+
   await app.listen(port);
   Logger.log(`Server is ready on http://localhost:${port}`)
 }
